7c01e07c6f7cac57cda7ccdb7eeb7a9861bc8142
syys1028 <syys10280@naver.com>
2025-04-09 23:34:33 +0900

    feat: PostTag 엔티티 설계 및 게시글-태그 연관관계 매핑

---
+++ board-service/src/main/java/com/backend/board_service/entity/PostTag.java
@@ -0,0 +1,26 @@
+package com.backend.board_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PostTag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tag_id")
+    private Tag tag;
+}
