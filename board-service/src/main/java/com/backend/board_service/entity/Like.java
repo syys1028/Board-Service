ffce46816e6d8887ba397de73064dfae2e97e9ad
syys1028 <syys10280@naver.com>
2025-04-09 23:42:09 +0900

    feat: 좋아요(Like) 기능 구현을 위한 Like 및 LikeId 엔티티 생성

---
+++ board-service/src/main/java/com/backend/board_service/entity/Like.java
@@ -0,0 +1,35 @@
+package com.backend.board_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@IdClass(LikeId.class)
+public class Like {
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    private LocalDateTime likedAt;
+
+    @PrePersist
+    protected void onLike() {
+        this.likedAt = LocalDateTime.now();
+    }
+}
---
+++ board-service/src/main/java/com/backend/board_service/entity/LikeId.java
@@ -0,0 +1,27 @@
+package com.backend.board_service.entity;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class LikeId implements Serializable {
+    private Long user;
+    private Long post;
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
