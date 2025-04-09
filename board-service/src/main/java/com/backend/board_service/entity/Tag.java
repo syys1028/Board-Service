00b81443dc1efcd03ab3ebbe7d9ca2d6223b5c3b
syys1028 <syys10280@naver.com>
2025-04-09 23:32:21 +0900

    feat: Tag 엔티티 설계

---
+++ board-service/src/main/java/com/backend/board_service/entity/Tag.java
@@ -0,0 +1,22 @@
+package com.backend.board_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;    // 태그 이름
+}
+
